c85af6b44e57f2fedf7643495290a9b05100dd82
emaduddin678 <emaduddin678@gmail.com>
2024-03-07 00:45:33 +0600

    bg done

--- script.js
+++ script.js
@@ -1,71 +1,75 @@
-const canvas = document.getElementById("canvas")
-const body = document.querySelector('body');
-canvas.height = window.innerHeight
-canvas.width = window.innerWidth
-var theColor = '';
+const canvas = document.getElementById("canvas");
+const body = document.querySelector("body");
+canvas.height = window.innerHeight  ;
+canvas.width = window.innerWidth ;
+var theColor = "";
 var lineW = 5;
-let prevX = null
-let prevY = null
-let draw = false
+let prevX = null;
+let prevY = null;
+let draw = false;
 
 body.style.backgroundColor = "#FFFFFF";
 var theInput = document.getElementById("favcolor");
 
-theInput.addEventListener("input", function(){
-  theColor = theInput.value;
-  body.style.backgroundColor = theColor;
-}, false);
+theInput.addEventListener(
+  "input",
+  function () {
+    theColor = theInput.value;
+    body.style.backgroundColor = theColor;
+  },
+  false
+);
 
-const ctx = canvas.getContext("2d")
+const ctx = canvas.getContext("2d");
 ctx.lineWidth = lineW;
 
-document.getElementById("ageInputId").oninput = function() {
-    draw = null
-    lineW = document.getElementById("ageInputId").value;
-    document.getElementById("ageOutputId").innerHTML = lineW;
-    ctx.lineWidth = lineW;
-};  
+document.getElementById("ageInputId").oninput = function () {
+  draw = null;
+  lineW = document.getElementById("ageInputId").value;
+  document.getElementById("ageOutputId").innerHTML = lineW;
+  ctx.lineWidth = lineW;
+};
 
-let clrs = document.querySelectorAll(".clr")
-clrs = Array.from(clrs)
-clrs.forEach(clr => {
-    clr.addEventListener("click", () => {
-        ctx.strokeStyle = clr.dataset.clr
-    })
-})
+let clrs = document.querySelectorAll(".clr");
+clrs = Array.from(clrs);
+clrs.forEach((clr) => {
+  clr.addEventListener("click", () => {
+    ctx.strokeStyle = clr.dataset.clr;
+  });
+});
 
-let clearBtn = document.querySelector(".clear")
+let clearBtn = document.querySelector(".clear");
 clearBtn.addEventListener("click", () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-})
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
 
-let saveBtn = document.querySelector(".save")
+let saveBtn = document.querySelector(".save");
 saveBtn.addEventListener("click", () => {
-    let data = canvas.toDataURL("imag/png")
-    let a = document.createElement("a")
-    a.href = data
-    a.download = "sketch.png"
-    a.click()
-})
+  let data = canvas.toDataURL("imag/png");
+  let a = document.createElement("a");
+  a.href = data;
+  a.download = "sketch.png";
+  a.click();
+});
 
-window.addEventListener("mousedown", (e) => draw = true)
-window.addEventListener("mouseup", (e) => draw = false)
+window.addEventListener("mousedown", (e) => (draw = true));
+window.addEventListener("mouseup", (e) => (draw = false));
 
 window.addEventListener("mousemove", (e) => {
-    if(prevX == null || prevY == null || !draw){
-        prevX = e.clientX
-        prevY = e.clientY
-        return
-    }
+  if (prevX == null || prevY == null || !draw) {
+    prevX = e.clientX;
+    prevY = e.clientY;
+    return;
+  }
 
-    let currentX = e.clientX
-    let currentY = e.clientY
+  let currentX = e.clientX;
+  let currentY = e.clientY;
 
-    ctx.beginPath()
-    ctx.moveTo(prevX, prevY)
-    ctx.lineTo(currentX, currentY)
-    ctx.stroke()
+  ctx.beginPath();
+  ctx.moveTo(prevX, prevY);
+  ctx.lineTo(currentX, currentY);
+  ctx.stroke();
 
-    prevX = currentX
-    prevY = currentY
-})
+  prevX = currentX;
+  prevY = currentY;
+});
